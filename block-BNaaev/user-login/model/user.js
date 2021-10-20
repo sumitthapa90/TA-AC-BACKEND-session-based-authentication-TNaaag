f3a4f5e9ab6ada1913afaf79ae93fb6071d007c9
sumit <sumitthapa90@gmail.com>
2021-10-20 18:33:36 +0530

    complete

---
+++ block-BNaaev/user-login/model/user.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number },
+    phone: { type: Number },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  if (this.password && this.isModified("password")) {
+    bcrypt.hash(this.password, 10, (err, hashed) => {
+      if (err) return next(err);
+      this.password = hashed;
+      return next();
+    });
+  } else {
+    next();
+  }
+});
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
